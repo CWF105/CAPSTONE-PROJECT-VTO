453812e4ce6be59ec6b3e7aa6577ffa3a21884d5
CWF105 <cw.flaviano105@gmail.com>
2024-09-17 08:52:53 +0800

    added new function

--- Assets/Scripts/button_functions.cs
+++ Assets/Scripts/button_functions.cs
@@ -1,8 +1,16 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
 
 public class button_functions : MonoBehaviour
 {
+
+    public void checkIfClickable_for_debbugging(Button button)
+    {
+        Debug.Log("Button "+ button +" is Clicked");
+    }
+
     // on click open url and close the app
     public void on_click_open_then_close(string url)
     {
@@ -32,4 +40,37 @@ public class button_functions : MonoBehaviour
             }
         }
     }
+
+
+
+public GameObject onClickAnimate1, onClickAnimate2;
+public Button btnToDisable1, btnToDisable2, clicked_btn_then_animate;
+public bool isClicked = false;
+
+public void on_click_use_digital_pen()
+{
+    if ((onClickAnimate1 != null) && (onClickAnimate2 != null)) 
+    {
+        Animator animate1 = onClickAnimate1.GetComponent<Animator>();
+        Animator animate2 = onClickAnimate2.GetComponent<Animator>();
+
+        if ((animate1 != null) && (animate2 != null)) 
+        {
+            // Toggle the animation states
+            bool isClicked_1 = animate1.GetBool("show");
+            bool isClicked_2 = animate2.GetBool("show");
+
+            animate1.SetBool("show", !isClicked_1);           
+            animate2.SetBool("show", !isClicked_2); 
+
+            // Toggle the boolean state `isClicked`
+            isClicked = !isClicked;
+
+            // Set the interactability of buttons based on `isClicked` state
+            btnToDisable1.interactable = !isClicked;
+            btnToDisable2.interactable = !isClicked;
+        }
+    }
+}
+
 }
